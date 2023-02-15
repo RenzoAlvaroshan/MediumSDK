16df3361f741e8fc2f05cf703cd00865d9a25b10
Renzo Alvaroshan <renzo.alvaroshan@gmail.com>
2023-02-16 06:18:43 +0700

    add functionalities

--- Sources/MediumSDK/MediumSDK.swift
+++ Sources/MediumSDK/MediumSDK.swift
@@ -1,6 +1,29 @@
-public struct MediumSDK {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import UIKit
+import WebKit
 
-    public init() {
-    }
+public class MediumSDK: WKWebView {
+	
+	public static var text: String = "Hello, World!"
+	
+	public init() {
+		let webConfiguration = WKWebViewConfiguration()
+		webConfiguration.preferences.javaScriptEnabled = true
+		super.init(frame: .zero, configuration: webConfiguration)
+		self.scrollView.isScrollEnabled = false
+		self.isMultipleTouchEnabled = false
+	}
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+	}
+	
+	@discardableResult
+	public func load() -> WKNavigation? {
+		if let url = URL(string:"http://www.apple.com") {
+			let req = URLRequest(url: url)
+			return super.load(req)
+		}
+		return nil
+	}
 }
--- Tests/MediumSDKTests/MediumSDKTests.swift
+++ Tests/MediumSDKTests/MediumSDKTests.swift
@@ -6,6 +6,6 @@ final class MediumSDKTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MediumSDK().text, "Hello, World!")
+		XCTAssertEqual(MediumSDK.text, "Hello, World!")
     }
 }
